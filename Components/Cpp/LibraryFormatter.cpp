dc348002931cdeeeb6d2c31a49b4602dfff1e6da
Niklas Hofmann <niklas.hofmann@everbase.net>
2014-10-08 14:03:22 +0200

    link current dummy code to library

--- Components/Cpp/LibraryFormatter.cpp
+++ Components/Cpp/LibraryFormatter.cpp
@@ -36,8 +36,18 @@ void LibraryFormatter::_definition(std::ostream& stream, Model::ClassRef class_)
     }
 
     stream
-        << name(class_) << "Impl::" << name(class_) << "Impl() { }" << endl
-        << name(class_) << "Impl::~" << name(class_) << "Impl() { }" << endl << endl;
+        << name(class_) << "Impl::" << name(class_) << "Impl()" << endl << "{" << endl;
+
+    filter(stream).push<reset>() << "#include \"dummy_impl/" << qname(class_, "/") << "/constructor_sync_impl.cpp\"" << endl;
+
+    stream
+        << "}" << endl << endl
+        << name(class_) << "Impl::~" << name(class_) << "Impl()" << endl << "{" << endl;
+
+    filter(stream).push<reset>() << "#include \"dummy_impl/" << qname(class_, "/") << "/destructor_sync_impl.cpp\"" << endl;
+
+    stream
+        << "}" << endl << endl;
 
     stream << "// " << name(class_) << ": }" << endl << endl;
 }
@@ -80,10 +90,12 @@ void LibraryFormatter::_definition(std::ostream& stream, Model::Class::Operation
 
     stream << ")" << endl << "{" << endl;
 
-    if (operation->result())
-    {
-        stream << "    return " << type(operation->result()->type()) << "();" << endl;
-    }
+//    if (operation->result())
+//    {
+//        stream << "    return " << type(operation->result()->type()) << "();" << endl;
+//    }
+
+    filter(stream).push<reset>() << "#include \"dummy_impl/" << qname(operation, "/") << "_sync_impl.cpp\"" << endl;
 
     stream << "}" << endl;
 }
