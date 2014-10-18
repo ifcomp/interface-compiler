9ba1e9588e92bbd41c5d235b064841aed9f87182
Niklas Hofmann <niklas.hofmann@everbase.net>
2014-10-19 01:21:34 +0200

    fix uuid

--- Components/Cpp/FormatterBase.cpp
+++ Components/Cpp/FormatterBase.cpp
@@ -46,7 +46,7 @@ FormatterBase::FormatterBase()
                 PrimitiveConfig<Primitive::Underlying::BOOLEAN>("bool"),
                 PrimitiveConfig<Primitive::Underlying::TIMESTAMP>("std::time_t"),
                 PrimitiveConfig<Primitive::Underlying::STRING>("std::string"),
-                PrimitiveConfig<Primitive::Underlying::UUID>("everbase::common::Uuid<>"),
+                PrimitiveConfig<Primitive::Underlying::UUID>("everbase::common::Uuid"),
                 PrimitiveConfig<Primitive::Underlying::OBJECTID>("everbase::common::ObjectId<%0>"),
                 PrimitiveConfig<Primitive::Underlying::VERSIONID>("everbase::common::VersionId")
             },
