feffaad4544208bacba2b0d1f64647cd0da83ef1
Niklas Hofmann <niklas.hofmann@everbase.net>
2014-10-29 11:46:04 +0100

    fix uuid init

--- Components/ObjectiveC/WrapperFormatter.cpp
+++ Components/ObjectiveC/WrapperFormatter.cpp
@@ -117,7 +117,7 @@ void WrapperFormatter::_definition(std::ostream& stream, Model::Class::ConstantR
                 {
                     auto uuid = boost::any_cast<boost::uuids::uuid>(constant->value());
 
-                    stream << "[NSUUID initWithUUIDBytes:(uint8_t[16]){ ";
+                    stream << "[[NSUUID alloc] initWithUUIDBytes:(uuid_t){ ";
 
                     for( auto i : indices(std::vector<std::uint8_t>(uuid.data, uuid.data + 16)) )
                     {
