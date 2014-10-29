ec25f2471a9a3b4c594a21313894aa44a6686f65
Niklas Hofmann <niklas.hofmann@everbase.net>
2014-10-29 11:39:16 +0100

    c99 type expression

--- Components/ObjectiveC/WrapperFormatter.cpp
+++ Components/ObjectiveC/WrapperFormatter.cpp
@@ -117,7 +117,7 @@ void WrapperFormatter::_definition(std::ostream& stream, Model::Class::ConstantR
                 {
                     auto uuid = boost::any_cast<boost::uuids::uuid>(constant->value());
 
-                    stream << "[NSUUID initWithUUIDBytes:{ ";
+                    stream << "[NSUUID initWithUUIDBytes:(" << type(constant->type()) << "){ ";
 
                     for( auto i : indices(std::vector<std::uint8_t>(uuid.data, uuid.data + 16)) )
                     {
